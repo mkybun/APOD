b0e07032bfbd425ba62fec09650620474aee08c4
Karina Zuniga <mkybun@gmail.com>
2021-09-20 13:08:46 -0500

    initial cleanup

--- client/components/Home.js
+++ client/components/Home.js
@@ -5,11 +5,10 @@ import {connect} from 'react-redux'
  * COMPONENT
  */
 export const Home = props => {
-  const {username} = props
 
   return (
     <div>
-      <h3>Welcome, {username}</h3>
+      <h3>Welcome!</h3>
     </div>
   )
 }
