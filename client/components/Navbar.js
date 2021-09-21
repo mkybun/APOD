3d46dc9124e8a18a229986a75afc0be0dfec9e5a
Karina Zuniga <mkybun@gmail.com>
2021-09-21 17:26:51 -0500

    add links

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -8,10 +8,10 @@ export default function Navbar(props) {
     <div>
       <p className='header-nav'>Astronomy Picture Of The Day</p>
       <p className='subtext'>ALL PHOTOS PROVIDED BY NASA, USING THE APOD NASA API</p>
-      <p className='subtext'>CREATED BY: </p>
-      <p className='subtext'>KARINA ZUNIGA</p>
-      {/* <nav>
-      </nav> */}
+      <p className='subtext'>CREATED BY: KARINA ZUNIGA </p>
+      <p className='subtext'>CHECK OUT THE CODE ON
+      <a className='subtext' id='link' href="https://github.com/mkybun/APOD">GITHUB</a> || KARINA'S <a className='subtext' id='link' href="https://www.linkedin.com/in/karina-zuniga/">LINKEDIN</a>
+      </p>
     </div>
   );
 }
